c70c3994aadd5d496c4cd8b9a927934d2897e985
shogo takata <bsi25019@uhe.ac.jp>
2025-09-19 14:22:49 +0900

    init

---
+++ Java2_1/src/Exp2_1.java
@@ -0,0 +1,11 @@
+
+public class Exp2_1 {
+
+	public static void main(String[] args) {
+		System.out.println("計算式:5×3-18÷6");
+		//System.out.println("計算結果を確認します");
+		System.out.println("計算結果は" + (5 * 3 - 18 / 6));
+		System.out.println("0より小さくなりませんでした");
+	}
+
+}
