606106e2f4b1e24839738bf746bcf6712f0814e9
shogo takata <bsi25019@uhe.ac.jp>
2025-09-22 12:10:26 +0900

    init

---
+++ Java2_3/src/Exp3_1.java
@@ -0,0 +1,11 @@
+
+public class Exp3_1 {
+
+	public static void main(String[] args) {
+		double d1 = 123.4;
+		String str = "567.8";
+		double d2 = Double.parseDouble(str);
+			System.out.println(d1 + d2);
+	}
+
+}
